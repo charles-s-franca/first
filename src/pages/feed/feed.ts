d48831d039968d184b995f9b8528a0d3eeed471a
Charles Franca <charlesfranca@Charless-MacBook-Air.local>
2017-07-13 11:17:09 -0300

    correção erro de build

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -7,7 +7,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See http://ionicframework.com/docs/components/#navigation for more info
  * on Ionic pages and navigation.
  */
-@IonicPage()
+
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html',
